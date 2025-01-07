81af3eb57284ad0268c8e9271f364c1ed9852e44
asungkim <asungkim007@naver.com>
2025-01-07 18:49:23 +0900

    extract num from string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,51 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        exam2();
+        exam4();
+    }
+
+    public static void exam4() {
+        String[] strNums = {"1번", "2번", "3번", "4번", "5번"};
+
+        int[] arr = Arrays.stream(strNums)
+                .map(str -> str.substring(0, 1))
+                .mapToInt(Integer::parseInt)
+                .filter(n -> n % 2 == 1)
+                .toArray();
+
+        Arrays.stream(arr).forEach(System.out::println);
     }
 
     public static void exam3() {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        // 값 2배 배열
-        // 값 3배 배열
-        // 값 10배 배열
+        System.out.println("=== 명령형 === ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * 2;
+        }
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("=== 스트림 ===");
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // 값 2배 배열
+        int[] arr2 = Arrays.stream(arr1)
+                .map(n -> 2 * n).toArray();
+
+        Arrays.stream(arr2).forEach(System.out::println);
+
+        // 값 3배 배열
+        int[] arr3 = Arrays.stream(arr1)
+                .map(n -> 3 * n).toArray();
+        Arrays.stream(arr3).forEach(System.out::println);
+
+        // 값 10배 배열
+        int[] arr4 = Arrays.stream(arr1)
+                .map(n -> 10 * n).toArray();
+        Arrays.stream(arr4).forEach(System.out::println);
     }
 
     public static void exam2() {
@@ -22,7 +57,7 @@ public class Main {
 
         System.out.println("=== 명령형 ===");
         int[] eveArr = new int[arr.length];
-        int idx=0;
+        int idx = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 eveArr[idx++] = arr[i];
@@ -34,18 +69,20 @@ public class Main {
         }
 
         System.out.println("=== 스트림 ===");
-        IntStream.rangeClosed(1,10)
+        int[] arr2 = IntStream.rangeClosed(1, 10)
                 .filter(Main::even)
-                .forEach(System.out::println);
+                .toArray();
+
+        System.out.println(arr2.length);
     }
 
     public static void exam1() {
         // 1부터 100까 만들고 짝수만 출력 - 스트림 이용
 
-        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("=== 명령형 ===");
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
 
@@ -57,7 +94,7 @@ public class Main {
         // 숫자 스트림을 바로 만들 수 있음.
         IntStream.rangeClosed(1, 100)
                 .forEach((num) -> {
-                    if(num % 2 == 0) {
+                    if (num % 2 == 0) {
                         System.out.println(num);
                     }
                 });
